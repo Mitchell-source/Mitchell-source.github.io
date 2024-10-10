ea62901b4e8b4c6ea5a2a6044f4d54f9afd3a9a9
Mitchell-source <mitchell.hammond@torontomu.ca>
2024-10-09 21:17:43 -0400

    more things

--- main.js
+++ main.js
@@ -1,30 +1,18 @@
-// Function to prompt the user for their name and display it on the homepage
-function promptUserName() {
-    let userName = prompt("Please enter your name:");
-    if (userName) {
-        document.getElementById('greeting').innerText = `Welcome, ${userName}!`;
-    }
+function ShowName() {
+    var name = document.getElementById("nameInput").value;
+    document.getElementById("greeting").innerHTML = "Hello, " + name + "!";
 }
 
-// Function to enlarge the image when clicked
 function enlargeImage() {
     let img = document.getElementById('profileImage');
     img.style.width = '300px'; 
     img.style.height = 'auto';
 }
 
-// Function to restore the image size
-function restoreImage() {
-    let img = document.getElementById('profileImage');
-    img.style.width = '150px'; 
-    img.style.height = 'auto';
-}
-
-// Function to change the color of navigation items on hover
 function changeNavColorOnHover(navItem) {
     navItem.style.backgroundColor = 'lightblue'; 
 }
 
-// Function to reset the color of navigation items on mouse out
 function resetNavColor(navItem) {
     navItem.style.backgroundColor = ''; 
+}
